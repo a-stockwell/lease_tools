6d09f1e10bb9770e5ca7af6eda92e11a9fac9dc7
Andy Stockwell <andy.stockwell@gmail.com>
2016-02-24 17:29:04 -0600

    first commit, text menu for 2 items in lease_calculators

---
+++ lease_calculators.py
@@ -0,0 +1,20 @@
+#This will be used to replace the Montly Calculator Excel spread sheet.
+#TO DO:
+#   Create Prorated Monthly due calculator
+#   Long Term Lease Montly Rates
+
+#Menu for access tools
+print("Welcome to the Absolutus 66, Inc. Lease Calculator")
+print("What would you like to do?")
+print("\t1. Find Prorated First Month's Rent")
+print("\t2. Calculate Long Term Lease Rates")
+print("\t3. Add new tenant to database")
+menu_selection = int(input("Please make a selection: "))
+
+#Go to menu selection
+if menu_selection == 1:
+    print("You selected to find prorated first months rental amount")
+    print("Stubbed out for now")
+elif menu_selection == 2:
+    print("You selected to calculate long term lease rates")
+    print("Stubbed Out for now")
